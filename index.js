ecdc57a37df1c1a09b807c29daa35f9b20f346ac
Brian Joseph <brianjoseph8132@gmail.com>
2024-09-26 23:57:43 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}console.log(mondayWork())
+
+function wrapAdjective(skip = "*"){
+    return function(adjective = "special"){
+        return `You are ${skip}${adjective}${skip}!`;
+    };
+}
